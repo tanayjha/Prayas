f75495d6c264b45582be58588c5c6e4235580afa
tanayjha <jha.tanay@gmail.com>
2017-02-05 10:44:11 +0530

    Bug fixes

--- prayas/students/views.py
+++ prayas/students/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from .forms import * 
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
+from main.models import *
 
 @require_http_methods(['GET', 'POST'])
 def addstudent(request):
 	if request.method == 'POST':
-		f = CreateStudentForm()
+		f = CreateStudentForm(request.POST)
 		if f.is_valid():
-			name = self.cleaned_data.get('name')
-			rollNo = self.cleaned_data.get('rollNo')
-			address = self.cleaned_data.get('address')
-			isActive = self.cleaned_data.get('isActive')
-			guardianName = self.cleaned_data.get('guardianName')
-			guardianPhone = self.cleaned_data.get('guardianPhone')
-			guardiansRelationWithChild = self.cleaned_data.get('guardiansRelationWithChild')
-			referenceName = self.cleaned_data.get('referenceName')
-			referencePhone = self.cleaned_data.get('referencePhone')
-			referenceAddress = self.cleaned_data.get('referenceAddress')
-			joiningDate = self.cleaned_data.get('joiningDate')
+			print ("HERE")
+			name = f.cleaned_data.get('name')
+			rollNo = f.cleaned_data.get('rollNo')
+			address = f.cleaned_data.get('address')
+			isActive = f.cleaned_data.get('isActive')
+			guardianName = f.cleaned_data.get('guardianName')
+			guardianPhone = f.cleaned_data.get('guardianPhone')
+			guardiansRelationWithChild = f.cleaned_data.get('guardiansRelationWithChild')
+			referenceName = f.cleaned_data.get('referenceName')
+			referencePhone = f.cleaned_data.get('referencePhone')
+			referenceAddress = f.cleaned_data.get('referenceAddress')
+			joiningDate = f.cleaned_data.get('joiningDate')
 
 
-			s = Students(name=name, rollNo=rollNo, address=address,
+			s = students(name=name, rollNo=rollNo, address=address,
 						 isActive=isActive, guardianName=guardianName, guardianPhone=guardianPhone,
 						 guardiansRelationWithChild=guardiansRelationWithChild, referenceName=referenceName,
 						 referencePhone=referencePhone, referenceAddress=referenceAddress, joiningDate=joiningDate)
@@ -31,10 +33,12 @@ def addstudent(request):
 			# user.save()
 			return render(request, 'students/student.html', data)
 		else:
+			print ("ELSE")
 			data = {'addstudentform' : f}
 			#return redirect('127.0.0.1:8000/warden/student#add')
 			return render(request, 'students/student.html', data)
 	else:
+		print ("NOW")
 		f = CreateStudentForm()
 		data = {'addstudentform' : f}
 		return render(request, 'students/student.html', data)
